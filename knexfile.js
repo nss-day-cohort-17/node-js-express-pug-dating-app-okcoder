e145da2be2b639768b3023fdedee8e1f70de9c43
Jared Fuller <jaredshane@gmail.com>
2017-04-11 13:35:08 -0500

    changed the staging and production client and database to postgres and okcoder

--- knexfile.js
+++ knexfile.js
@@ -2,48 +2,49 @@
 
 module.exports = {
 
- development: {
-   client: 'postgresql',
-   connection: {
-     database: 'okcoder'
-   },
-   migrations: {
-     directory: __dirname + '/db/migrations'
-   },
-   seeds: {
-     directory: __dirname + '/db/seeds'
-   }
- },
+  development: {
+    client: 'postgresql',
+    connection: {
+      database: 'okcoder'
+    },
+    migrations: {
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds'
+    }
+  },
 
- staging: {
-   client: 'postgresql',
-   connection: {
-     database: 'my_db',
-     user:     'username',
-     password: 'password'
-   },
-   pool: {
-     min: 2,
-     max: 10
-   },
-   migrations: {
-     tableName: 'knex_migrations'
-   }
- },
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'okcoder',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
 
- production: {
-   client: 'postgresql',
-   connection: {
-     database: 'my_db',
-     user:     'username',
-     password: 'password'
-   },
-   pool: {
-     min: 2,
-     max: 10
-   },
-   migrations: {
-     tableName: 'knex_migrations'
-   }
- }
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'okcoder',
+      user:     'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
+
 };
