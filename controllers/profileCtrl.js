9544b376da1db58d89d70bd0fc84d7ea05226275
Luke Schuyler <lukeschuyler1@gmail.com>
2017-04-12 13:58:59 -0500

    added res.locals.email = app.locals.email

--- controllers/profileCtrl.js
+++ controllers/profileCtrl.js
@@ -1,4 +1,9 @@
 'use strict';
 
-module.exports.show = (req, res) =>
+const User = require('../models/userMod')
+
+module.exports.show = (req, res) => {
+  console.log(res.locals.email, 'profile email')
+  // User.findOneByEmail(email)
   res.render('profile');
+}
