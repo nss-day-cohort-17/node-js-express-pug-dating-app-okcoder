065578f025a0e807866451382bad77ab28c3c44e
Jared Fuller <jaredshane@gmail.com>
2017-04-13 11:29:33 -0500

    added function to like or match with a user

--- controllers/profileCtrl.js
+++ controllers/profileCtrl.js
@@ -20,3 +20,44 @@ module.exports.showOther = (req, res) => {
       res.render('otherProfile', {user});
     })
 }
+
+
+//checking to see where the user has been liked by another user so that can be checked for matches
+
+const GetWhereUserIsLiked = (userId, paramsId) => {
+  return Like.forge().where({likee: userId, liker: paramsId}).fetch({columns: ['liker', 'likee']})
+}
+
+module.exports.likeUser = (req, res, err) => {
+  console.log("user", req.user.id)
+  console.log("params", req.params.id)
+  GetWhereUserIsLiked(req.user.id, req.params.id)
+    .then( (user) => {
+      console.log('user return', user)
+      // user.toJSON())
+      if (user !== null) {
+        console.log('match stuff')
+        return user.toJSON()
+        console.log("user.toJSON()", user.toJSON());
+      }
+    })
+    .then( (user) => {
+      console.log('thenuser', user)
+      if (user) {
+        //match the users
+        Match.forge({userOne: user.likee, userTwo: user.liker})
+          .save()
+          .then( () => {
+            console.log('done with match')
+        })
+      }
+      else {
+        //post a like
+        Like.forge({liker: req.user.id, likee: req.params.id})
+          .save()
+          .then( () => {
+            console.log('done with like')
+          })
+      }
+    })
+}
